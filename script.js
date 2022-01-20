cdfa380c92654a65c1825a285d2291ac7fdade8e
DanielGebre2022 <91226945+DanielGebre2022@users.noreply.github.com>
2022-01-19 18:11:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+//set the variables for the number for greeting and activity
+let greeting = 'Welcome to my calculator'
+let firstNum = 5
+let secondNum = 7
+let total = firstNum + secondNum 
+let activity = `The sum of ${firstNum} + ${secondNum} is : ${total}`
+
+//set variables for selectors
+let firstP = document.getElementById('firstP')
+let secondP = document.getElementById('secondP')
+
+//inserted variable into the objects
+firstP.innerHTML = greeting
+secondP.innerHTML = activity
